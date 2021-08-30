da699cff210a09e07ca8036962c3e2deacce7fca
ldcave <ldcave@mix.wvu.edu>
2021-08-29 21:34:07 -0400

    Update command_handler.c
    added mpx_supt file & mpx init

--- mpx_core/kernel/core/command_handler.c
+++ mpx_core/kernel/core/command_handler.c
@@ -5,7 +5,7 @@
 #include <core/serial.h>
 #include <core/command_handler.h>
 
-
+#include "modules/mpx_supt.h"
 
 
 int run_ch() {
@@ -13,7 +13,8 @@ int run_ch() {
   ch_running = 1;
   char commandBuff[100];
   int commandSize;
-
+  mpx_init(MODULE_R1);
+  
   serial_println("Welcome to DiamondDogs OS");
   serial_println("What command would you like to run?");
 
@@ -24,8 +25,6 @@ int run_ch() {
     commandSize = 99;
     memset (buffer,'\0',100);
     sys_req(READ,DEFAULT_DEVICE,commandBuff,&commandSize);
-
-
 
   }
 
