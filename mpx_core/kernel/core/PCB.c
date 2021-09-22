11c28a5b255889f3f16994986b683abfbb294800
Jacob Taylor <jct0020@mix.wvu.edu>
2021-09-22 15:41:35 -0400

    Assignment to Expression with Array Type Error

--- mpx_core/kernel/core/PCB.c
+++ mpx_core/kernel/core/PCB.c
@@ -8,13 +8,15 @@
 #include <core/PCB.h>
 
 struct PCB* AllocatePCB(){
+  struct PCB* Pcb;
   sys_alloc_mem(1084);
-  return AllocatePCB;
+  return Pcb;
 }
 
 struct PCB* FindPCB(char* name){
-  if(strcmp(&FindPCB->process_name, name) == 0){
-    return FindPCB;
+  struct PCB* Pcb;
+  if(strcmp(Pcb->process_name, name) == 0){
+    return Pcb;
   }
     return NULL;
 }
@@ -25,24 +27,27 @@ struct PCB* FreePCB(struct PCB* pcbName){
 }
 
 struct PCB* SetupPCB(char* processName, int class, int priority){
+  struct PCB* pcb;
   AllocatePCB();
-  SetupPCB->process_name = processName;
-  SetupPCB->process_class = class;
-  SetupPCB->priority = priority;
-  return SetupPCB;
+  pcb->process_name = processName;
+  pcb->process_class = class;
+  pcb->priority = priority;
+  return pcb;
 }
 
 struct PCB* InsertPCB(struct PCB* pcbName){
   //Ready Or Blocked
-  pcbName->next = InsertPCB;
-  if(strcmp(pcbName->processName, pcbName)!= 0){
+  struct PCB* pcb;
+  pcbName->next = pcb;
+  if(strcmp(pcbName->process_name, pcb->process_name)!= 0){
     return NULL;
   }
-  return InsertPCB;
+  return pcb;
 }
 
 struct PCB* RemovePCB(struct PCB* pcbName){
   //Ready Or Blocked
-  RemovePCB->previous = pcbName;
+  struct PCB* pcb;
+  pcb->previous = pcbName;
   return pcbName;
 }
