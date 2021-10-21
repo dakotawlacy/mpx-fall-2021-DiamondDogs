553d2c013254f74afbe86c3e2378c24867775d4e
Easton <theweseaston@gmail.com>
2021-10-21 18:58:52 -0400

    settime

--- mpx_core/kernel/core/commands/settime.c
+++ mpx_core/kernel/core/commands/settime.c
@@ -45,7 +45,7 @@ int run_settime(char * commandBuff) {
   int sec = atoi(seconds);
 
   //Error checking
-  if (hrs > 12 || hrs < 0 || mins > 59 || mins < 0 || sec > 59 || sec < 0) {
+  if (hrs > 23 || hrs < 0 || mins > 59 || mins < 0 || sec > 59 || sec < 0) {
     return 1;
   }
 
